58e6ecded2e34e7fe474e66a1a4ef53b37a2b3af
Ura427 <urakost427@gmail.com>
2023-09-27 18:58:41 +0300

    add btn Продати авто

--- src/pages/cars-list/CarsList.jsx
+++ src/pages/cars-list/CarsList.jsx
@@ -54,133 +54,183 @@ const carData = [
 
 const CarsList = () => {
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-        <div style={{ height: 560, width: 240, backgroundColor: 'rgba(211, 211, 211, 0.1)', margin: 16 }}>
-            <h1>Фільтр авто</h1>
+    <div>
+      <div style={{ display: 'flex', justifyContent:'end'}}>
+        <Button
+          style={{
+            backgroundColor: "#e78e36",
+            color: "white",
+            fontSize: 20,
+            margin: 10
+          }}
+        >
+          Продати авто
+        </Button>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{
+            height: 560,
+            width: 240,
+            backgroundColor: "rgba(211, 211, 211, 0.1)",
+            margin: 16,
+          }}
+        >
+          <h1>Фільтр авто</h1>
 
-            <div style={{ margin: 5}}>
-                <label style={{ fontWeight: 'bold'}}>Стан авто</label>
-                <div>
-                    <input type="checkbox" ></input>
-                    <label>Б/У</label>
-                </div>
-                <div>
-                    <input type='checkbox' ></input>
-                    <label>Нові</label>
-                </div>
-               
+          <div style={{ margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Стан авто</label>
+            <div>
+              <input type="checkbox"></input>
+              <label>Б/У</label>
             </div>
-
-
-            <div style={{ margin: 5}}>
-                <label style={{ fontWeight: 'bold'}}>Об'єм двигуна</label>
-                <div style={ {display:'flex', gap: 10 }}>
-                    <input type='number' min="0" placeholder="від" style={{ width: '100%', boxSizing: 'border-box' }}></input>
-                    <input type="number" min="0" placeholder="до" style={{ width: '100%', boxSizing: 'border-box' }}></input>
-                </div>
-               
+            <div>
+              <input type="checkbox"></input>
+              <label>Нові</label>
             </div>
+          </div>
 
-            <div style={{ margin: 5}}>
-                <label style={{ fontWeight: 'bold'}}>Рік</label>
-                <div style={{ display: 'flex', gap: 10 }}>
-                <input type='number' min="0" placeholder="від" style={{ width: '100%', boxSizing: 'border-box' }} ></input>
-                <input type="number" min="0" placeholder="до" style={{ width: '100%', boxSizing: 'border-box' }}></input>
-                </div>
+          <div style={{ margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Об'єм двигуна</label>
+            <div style={{ display: "flex", gap: 10 }}>
+              <input
+                type="number"
+                min="0"
+                placeholder="від"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
+              <input
+                type="number"
+                min="0"
+                placeholder="до"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
             </div>
-            
-            <div style={{ margin: 5}}>
-                <label style={{ fontWeight: 'bold'}}>Пробіг(тис. км)</label>
-                <div style={{ display: 'flex', gap: 10 }}>
-                    <input type='number' min="0" placeholder="від" style={{ width: '100%', boxSizing: 'border-box' }}></input>
-                    <input type="number" min="0" placeholder="до" style={{ width: '100%', boxSizing: 'border-box' }}></input>
-                </div>
+          </div>
+
+          <div style={{ margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Рік</label>
+            <div style={{ display: "flex", gap: 10 }}>
+              <input
+                type="number"
+                min="0"
+                placeholder="від"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
+              <input
+                type="number"
+                min="0"
+                placeholder="до"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
             </div>
+          </div>
 
-            <div style={{ display: 'flex', flexDirection:'column', margin: 5}}>
-                <label style={{ fontWeight:'bold' }}>Марка</label>
-                <select>
-                    <option>Mercedes</option>
-                    <option>Audi</option>
-                    <option>Skoda</option>
-                    <option>Volkswagen</option>
-                </select>
+          <div style={{ margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Пробіг(тис. км)</label>
+            <div style={{ display: "flex", gap: 10 }}>
+              <input
+                type="number"
+                min="0"
+                placeholder="від"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
+              <input
+                type="number"
+                min="0"
+                placeholder="до"
+                style={{ width: "100%", boxSizing: "border-box" }}
+              ></input>
             </div>
+          </div>
 
-            <div style={{ display: 'flex', flexDirection: 'column', margin: 5}}>
-                <label style={{ fontWeight: 'bold'}}>Модель</label>
-                <select>
+          <div style={{ display: "flex", flexDirection: "column", margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Марка</label>
+            <select>
+              <option>Mercedes</option>
+              <option>Audi</option>
+              <option>Skoda</option>
+              <option>Volkswagen</option>
+            </select>
+          </div>
 
-                </select>
+          <div style={{ display: "flex", flexDirection: "column", margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Модель</label>
+            <select></select>
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column", margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Область</label>
+            <select>
+              <option>Львівська</option>
+              <option>Київська</option>
+              <option>Луганська</option>
+              <option>Житомирська</option>
+              <option>Запорізька</option>
+            </select>
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column", margin: 5 }}>
+            <label style={{ fontWeight: "bold" }}>Коробка передач</label>
+            <div>
+              <input type="checkbox"></input>
+              <label>Ручна / Механіка</label>
             </div>
-
-            <div style={{ display: 'flex', flexDirection: 'column', margin: 5 }}>
-                <label style={{ fontWeight: 'bold'}}>Область</label>
-                <select>
-                    <option>Львівська</option>
-                    <option>Київська</option>
-                    <option>Луганська</option>
-                    <option>Житомирська</option>
-                    <option>Запорізька</option>
-                </select>
+            <div>
+              <input type="checkbox"></input>
+              <label>Автомат</label>
             </div>
+          </div>
 
-            <div style={{ display: 'flex', flexDirection: 'column', margin: 5}}>
-                <label style={{ fontWeight: 'bold' }}>Коробка передач</label>
-                <div>
-                    <input type='checkbox'></input>
-                    <label>Ручна / Механіка</label>
-                </div>
-               <div>
-                <input type='checkbox'></input>
-                    <label>Автомат</label>
-               </div>
-            </div>
-
-            
-
-            <div style={{ margin: 5 }}>
-                <Button style={{ fontWeight: 'bold'}}>
-                    Search
-                </Button>
-            </div>
-           
+          <div style={{ margin: 5 }}>
+            <Button style={{ fontWeight: "bold" }}>Search</Button>
+          </div>
         </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          alignItems: "center",
-        }}
-      >
-        {carData.map((car) => (
-          <Card key={car.id} style={{ width: 760, margin: 16, display: "flex" }}>
-            <img
-              src={car.image}
-              alt={car.name}
-              style={{ width: 380, height: 250, objectFit: "cover" }}
-            />
-            <CardContent style={{display: 'flex', flexDirection:'column', justifyContent: 'space-between'}}>
-              <Typography
-                gutterBottom
-                variant="h6"
-                component="div"
-                style={{ textOverflow: "ellipsis", whiteSpace: "nowrap" }}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            alignItems: "center",
+          }}
+        >
+          {carData.map((car) => (
+            <Card
+              key={car.id}
+              style={{ width: 760, margin: 16, display: "flex" }}
+            >
+              <img
+                src={car.image}
+                alt={car.name}
+                style={{ width: 380, height: 250, objectFit: "cover" }}
+              />
+              <CardContent
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
               >
-                {car.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {car.description}
-              </Typography>
-              <CardActions style={{ marginTop: 'auto' }}>
-                <Button size="small" color="primary">
-                  More
-                </Button>
-              </CardActions>
-            </CardContent>
-          </Card>
-        ))}
+                <Typography
+                  gutterBottom
+                  variant="h6"
+                  component="div"
+                  style={{ textOverflow: "ellipsis", whiteSpace: "nowrap" }}
+                >
+                  {car.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {car.description}
+                </Typography>
+                <CardActions style={{ marginTop: "auto" }}>
+                  <Button size="small" color="primary">
+                    More
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
