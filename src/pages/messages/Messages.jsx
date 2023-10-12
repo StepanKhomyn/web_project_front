17bb3bb84615b1cc7b0bc250fe80d6b1ba7a971f
Ura427 <urakost427@gmail.com>
2023-10-12 20:06:43 +0300

    create messages page

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,9 +1,206 @@
-import React from 'react'
+import React from "react";
+import "./Messages.css";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Messages = () => {
   return (
-    <div>Messages</div>
-  )
-}
+    <div id="main">
+      <h2>Всі повідомлення</h2>
+      
+      <div className="main-container">
+      <div className="chats">
+     
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
 
-export default Messages
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+
+          <a href="#" className="chat-card">
+            <div>
+              <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+              <div className="seller-name">Ігор</div>
+              <div className="car-name">Tesla Model X 2022</div>
+              <div className="last-message">Lorem ipsum....</div>
+            </div>
+          </a>
+        
+      </div>
+
+      <div className="chat-container">
+        <div className="chat-container-seller"> 
+            <AccountCircleIcon/> Ігор 
+        </div>
+        <div className="line-separator"></div>
+
+
+        <div className="messages">
+          <div className="message message-r">
+            Доброго дня, це авто ще в наявності?
+          </div>
+          <div className="message message-l">
+            Доброго дня, так
+          </div>
+          <div className="message message-r">
+            Я б хотів його оглянути 
+          </div>
+          <div className="message message-r">
+            Коли можна було б зустрітися? 
+          </div>
+          <div className="message message-l">
+            Можемо детальніше все обгорорити за телефоном
+          </div>
+          <div className="message message-l">
+            +38067*******
+          </div>
+        </div>
+
+        <div className="line-separator"></div>
+
+        <div className="message-input-container">
+          <input type="text" className="message-input" placeholder="Напишіть повідомлення"></input>
+        </div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Messages;
