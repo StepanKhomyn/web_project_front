e97ff136c2f0432940594967aec8e70a60ddf54d
Ura427 <urakost427@gmail.com>
2023-10-20 10:22:30 +0300

    fixed minor troubles

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -24,7 +24,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -35,19 +35,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
-                </div>
-              </a>
-
-
-              <a href="#" className="chat-card">
-                <div>
-                  <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
-                </div>
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <div className="seller-name">Ігор</div>
-                  <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -59,18 +47,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
-                </div>
-              </a>
-
-              <a href="#" className="chat-card">
-                <div>
-                  <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
-                </div>
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <div className="seller-name">Ігор</div>
-                  <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -82,7 +59,18 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
+                </div>
+              </a>
+
+              <a href="#" className="chat-card">
+                <div>
+                  <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <div className="seller-name">Ігор</div>
+                  <div className="car-name">Tesla Model X 2022</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -94,7 +82,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -106,7 +94,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -118,7 +106,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -130,7 +118,19 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
+                </div>
+              </a>
+
+
+              <a href="#" className="chat-card">
+                <div>
+                  <img className="chat-img" src={'https://cdn3.riastatic.com/photosnew/auto/photo/tesla_model-3__515756233f.webp'}></img>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <div className="seller-name">Ігор</div>
+                  <div className="car-name">Tesla Model X 2022</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -141,7 +141,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -152,7 +152,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -163,7 +163,7 @@ const Messages = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className="seller-name">Ігор</div>
                   <div className="car-name">Tesla Model X 2022</div>
-                  <div className="last-message">Lorem ipsum....</div>
+                  <div className="last-message">Мій: +38099*******</div>
                 </div>
               </a>
 
@@ -184,16 +184,55 @@ const Messages = () => {
                   Доброго дня, так
                 </div>
                 <div className="message message-r">
-                  Я б хотів його оглянути
+                  Яка причина продажу?
+                </div>
+                <div className="message message-l">
+                  Покатався на теслі і зрозумів, що це не моє
+                </div>
+                <div className="message message-l">
+                  До того ж, мені часто доводиться їздити по областях у зв'язку із роботою  
+                </div>
+                <div className="message message-l">
+                  А для такого це авто явно не розраховане  
                 </div>
                 <div className="message message-r">
-                  Коли можна було б зустрітися?
+                  Зрозуміло
+                </div>
+                <div className="message message-r">
+                  Чи буде торг?
                 </div>
                 <div className="message message-l">
-                  Можемо детальніше все обгорорити за телефоном
+                  Ні, оскільки авто майже з салону
+                </div>
+                <div className="message message-r">
+                  Навіть 100$ не опустите?
                 </div>
                 <div className="message message-l">
-                  +38067*******
+                  За 100$ ще можна буде домовитися  
+                </div>
+                <div className="message message-r">
+                  Чудово)
+                </div>
+                <div className="message message-r">
+                  Коли б можна було під'їхати та оглянути авто?  
+                </div>
+                <div className="message message-l">
+                  Я вільний на цих вихідних
+                </div>
+                <div className="message message-r">
+                  Добре, я буду десь біля обіду 
+                </div>
+                <div className="message message-l">
+                  Добре, чекатиму  
+                </div>
+                <div className="message message-r">
+                  Залиште свій номер телефону щоб у разі чого можна було подзвонити
+                </div>
+                <div className="message message-l">
+                +38067*******
+                </div>
+                <div className="message message-r">
+                  Мій: +38099*******
                 </div>
               </div>
 
@@ -201,6 +240,11 @@ const Messages = () => {
 
               <div className="message-input-container">
                 <input type="text" className="message-input" placeholder="Напишіть повідомлення"></input>
+                <div className="message-send-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+<path d="M11.5003 12H5.41872M5.24634 12.7972L4.24158 15.7986C3.69128 17.4424 3.41613 18.2643 3.61359 18.7704C3.78506 19.21 4.15335 19.5432 4.6078 19.6701C5.13111 19.8161 5.92151 19.4604 7.50231 18.7491L17.6367 14.1886C19.1797 13.4942 19.9512 13.1471 20.1896 12.6648C20.3968 12.2458 20.3968 11.7541 20.1896 11.3351C19.9512 10.8529 19.1797 10.5057 17.6367 9.81135L7.48483 5.24303C5.90879 4.53382 5.12078 4.17921 4.59799 4.32468C4.14397 4.45101 3.77572 4.78336 3.60365 5.22209C3.40551 5.72728 3.67772 6.54741 4.22215 8.18767L5.24829 11.2793C5.34179 11.561 5.38855 11.7019 5.407 11.8459C5.42338 11.9738 5.42321 12.1032 5.40651 12.231C5.38768 12.375 5.34057 12.5157 5.24634 12.7972Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                </div>
               </div>
             </div>
           </div>
