4273e57ae537745c6ae359cc6d69c64c6f73ddff
Ura427 <urakost427@gmail.com>
2023-11-02 11:47:55 +0200

    create component for displaying chats

---
+++ src/pages/messages/messages-components/ChatListItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Messages.css';
+
+function ChatListItem(props) {
+  return (
+    <>
+      <a href="#" className={`chat-card ${props.isLastClicked ? 'last-clicked' : ''}`} onClick={props.onClick}>
+        <div>
+          <img className="chat-img" src={props.carImg} alt={props.carName} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div className="seller-name">{props.sellerName}</div>
+          <div className="car-name">{props.carName}</div>
+          <div className="last-message">{props.lastMessage}</div>
+        </div>
+      </a>
+    </>
+  );
+}
+
+export default ChatListItem;
